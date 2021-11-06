217fe06ff6c5d14f2dbba6d15e267a86bc1956b2
natalie <nataliegoncalvesdasilva02@gmail.com>
2021-11-05 21:21:30 -0300

    resolução do exercício soma2numeros

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace soma2numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("--- Operações com inteiros");
+            Console.WriteLine("Calcule a soma entre dois números.");
+
+            Console.Write("Número 1: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Número 2: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int soma =n1 + n2;
+
+            Console.WriteLine($"{n1} + {n2} = {soma}");
+        }
+    }
+}
